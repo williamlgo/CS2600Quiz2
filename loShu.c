184ad80848a7482c223d5658d46d38f65cee0a5d
William Go <wlgo@cpp.edu>
2022-03-10 19:26:34 -0800

    Updated code

--- loShu.c
+++ loShu.c
@@ -1,9 +1,12 @@
 //Lo Shu Square
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
 
 //Functions
 bool checkExist(int num);
-bool checkSum(int arr[][]);
+bool checkSum(int arr[3][3]);
 void resetExist();
 int main();
 
@@ -87,11 +90,12 @@ bool checkExist(int num){
       nine = true;
       return true;
     }
+  }
 }
 
 //Checks the sum of each row column, AND DIAGONAL if they equal to 15
-bool checkSum(){
-    bool magicSquare = true
+bool checkSum(int arr[3][3]){
+    bool magicSquare = true;
     //Checking column
     if((arr[0][0] + arr[0][1] + arr[0][2]) != 15)
         magicSquare = false;
@@ -134,35 +138,44 @@ int main(){
   bool done = false;
   int arr[3][3];
   int i,j;
-  while done == false{
+  srand(time(NULL));
+  printf("starting");
+
+  while (done == false){
     //Generating and filling the 2d array
-    for(int i=0; i<3, i++){
-        for(int j=0; i<3, j++){
+    for(int i=0; i<3; i++){
+        for(int j=0; i<3; j++){
             do{
-            arr[i][j] = RANDOM INSERT HERE;
-            }while(checkSum(arr[i][j] == false);
+            arr[i][j] = (rand() % 9) + 1;
+            printf("checking");
+            }while(checkExist(arr[i][j]) == false);
         }
     }
+    printf("done check sum time");
     //Checking if it is a lo shu square
-    if(checkSum(arr[][]_){
+    if(checkSum(arr)){
         done = true;
     }else{
         //Create a new empty array to reset arr
-        newArr[3][3];
-        arr[][] = newArr[][];
+        for(int i=0; i<3; i++){
+          for(int j=0; i<3; j++){
+              arr[i][j] = 0;
+          }
+        }
         resetExist();
+        printf("just reset");
     }
     counter++; //counting how many times to make a lo shu square
   }
   //Print out the array
-    for(int i=0; i<3, i++){
-        for(int j=0; i<3, j++){
+    for(int i=0; i<3; i++){
+        for(int j=0; i<3; j++){
             if(j == 1){
-            printf("[" + arr[i][j]);
+            printf("[%d ", arr[i][j]);
         }else if(j == 2){
-            printf(arr[i][j] + "]");
+            printf(" %d]", arr[i][j]);
         }else{
-            printf(arr[i][j]);
+            printf("%d", arr[i][j]);
         }
         }
     }
