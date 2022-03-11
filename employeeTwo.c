3cf78682730836eef62045373977edc9906ff7a0
William Go <wlgo@cpp.edu>
2022-03-10 19:43:33 -0800

    Corrected code

--- employeeTwo.c
+++ employeeTwo.c
@@ -1,6 +1,6 @@
 //employeeTwo.c
-#include <string.h> 
-#include "employee.h" 
+#include <string.h> 
+#include "employee.h" 
 //NOTE: 5 functions have been defined below 
 //ptr - points to table to be searched 
 //tableSize - size of the table 
@@ -11,32 +11,32 @@
 //What does setting a function to static do??  
 //functionPtr is the address of the function just use the name 
 //()(The second set of parentheses is the arguments of the function call) 
-static PtrToEmployee searchEmployeeTable(PtrToConstEmployee ptr, int tableSize, const void *targetPtr,  
-                                        int (*functionPtr)(const void *, PtrToConstEmployee)) 
+static PtrToEmployee searchEmployeeTable(PtrToConstEmployee ptr, int tableSize, const void *targetPtr,  
+        int (*functionPtr)(const void *, PtrToConstEmployee)) 
 { 
-    PtrToConstEmployee endPtr = ptr + tableSize; 
-    for(; ptr < endPtr; ptr++) 
-        if ((*functionPtr)(targetPtr, ptr) == 0) 
-            return (PtrToEmployee) ptr; 
-    return NULL; 
+    PtrToConstEmployee endPtr = ptr + tableSize; 
+    for(; ptr < endPtr; ptr++) 
+        if ((*functionPtr)(targetPtr, ptr) == 0) 
+            return (PtrToEmployee) ptr; 
+    return NULL; 
 } 
 //The functionPtr will point to one of these comparison functions to perform a check 
-static int compareEmployeeNumber(const void *targetPtr, PtrToConstEmployee tableValuePtr) 
+static int compareEmployeeNumber(const void *targetPtr, PtrToConstEmployee tableValuePtr) 
 { 
-    return * (long *) targetPtr != tableValuePtr->number; //const void *targetPtr ==> typecast as int pointer then dereference 
+    return * (long *) targetPtr != tableValuePtr->number; //const void *targetPtr ==> typecast as int pointer then dereference 
 } 
-static int compareEmployeeName(const void *targetPtr, PtrToConstEmployee tableValuePtr) 
+static int compareEmployeeName(const void *targetPtr, PtrToConstEmployee tableValuePtr) 
 { 
-    return strcmp((char *) targetPtr, tableValuePtr->name); //const void *targetPtr ==> typecast as char pointer then pass into strcmp() 
+    return strcmp((char *) targetPtr, tableValuePtr->name); //const void *targetPtr ==> typecast as char pointer then pass into strcmp() 
 }  
 
  
 //These are called wrappers. These functions are what you will use in your main!!! 
-PtrToEmployee searchEmployeeByNumber(PtrToConstEmployee ptr, int size, long number) 
+PtrToEmployee searchEmployeeByNumber(PtrToConstEmployee ptr, int size, long number) 
 { 
-    return searchEmployeeTable(ptr, size, &number, compareEmployeeNumber); 
+    return searchEmployeeTable(ptr, size, &number, compareEmployeeNumber); 
 } 
-PtrToEmployee searchEmployeeByName(PtrToConstEmployee ptr, int size, char* name) 
+PtrToEmployee searchEmployeeByName(PtrToConstEmployee ptr, int size, char* name) 
 { 
-    return searchEmployeeTable(ptr, size, name, compareEmployeeName); 
+    return searchEmployeeTable(ptr, size, name, compareEmployeeName); 
 } 
